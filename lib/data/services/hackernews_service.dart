6064d25fe922a42fc846869c22b9305df27dd11e
Andrey M <meamka@ya.ru>
2025-01-20 01:41:01 +0200

    Extend HN service add new and best stories methods

--- lib/data/services/hackernews_service.dart
+++ lib/data/services/hackernews_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
 import "../models/item.dart";
@@ -26,7 +27,58 @@ class HackernewsService {
         final json = jsonDecode(stringData) as List<dynamic>;
         return Result.ok(List<int>.from(json));
       } else {
-        throw Exception('Failed to load top stories');
+        log('Failed to best top stories');
+        return const Result.error(HttpException("Invalid response"));
+      }
+    } on Exception catch (error) {
+      return Result.error(error);
+    } finally {
+      client.close();
+    }
+  }
+
+  /// Fetches the list of new story IDs from Hacker News.
+  ///
+  /// Returns a [Result] containing either the list of story IDs or an error.
+  Future<Result<List<int>>> getNewStories() async {
+    final client = _clientFactory();
+
+    final url = Uri.parse('$_host/newstories.json');
+    try {
+      final request = await client.getUrl(url);
+      final response = await request.close();
+      if (response.statusCode == HttpStatus.ok) {
+        final stringData = await response.transform(utf8.decoder).join();
+        final json = jsonDecode(stringData) as List<dynamic>;
+        return Result.ok(List<int>.from(json));
+      } else {
+        log('Failed to new stories');
+        return const Result.error(HttpException("Invalid response"));
+      }
+    } on Exception catch (error) {
+      return Result.error(error);
+    } finally {
+      client.close();
+    }
+  }
+
+  /// Fetches the list of best story IDs from Hacker News.
+  ///
+  /// Returns a [Result] containing either the list of story IDs or an error.
+  Future<Result<List<int>>> getBestStories() async {
+    final client = _clientFactory();
+
+    final url = Uri.parse('$_host/beststories.json');
+    try {
+      final request = await client.getUrl(url);
+      final response = await request.close();
+      if (response.statusCode == HttpStatus.ok) {
+        final stringData = await response.transform(utf8.decoder).join();
+        final json = jsonDecode(stringData) as List<dynamic>;
+        return Result.ok(List<int>.from(json));
+      } else {
+        log('Failed to load best stories');
+        return const Result.error(HttpException("Invalid response"));
       }
     } on Exception catch (error) {
       return Result.error(error);
@@ -49,7 +101,8 @@ class HackernewsService {
         final json = jsonDecode(stringData) as Map<String, dynamic>;
         return Result.ok(Item.fromJson(json));
       } else {
-        throw Exception('Failed to load item');
+        log('Failed to load item with id $id');
+        return const Result.error(HttpException("Invalid response"));
       }
     } on Exception catch (error) {
       return Result.error(error);
