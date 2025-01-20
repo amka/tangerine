6dc7a8c1f9c6654ffb972ce883f9e51e1f913dc5
Andrey M <meamka@ya.ru>
2025-01-20 02:25:53 +0200

    Add display for a top stories

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hugeicons/hugeicons.dart';
 
+import '../core/app_title.dart';
+import '../../data/models/item.dart';
+import '../../data/repository/item_repository.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -9,16 +13,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final itemRepo = ItemRepository();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.primary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -43,64 +44,36 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: false,
         toolbarHeight: 80,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class AppTitle extends StatelessWidget {
-  const AppTitle({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: Theme.of(context).colorScheme.onSurface,
-            borderRadius: BorderRadius.circular(4),
-          ),
-          width: 36,
-          height: 36,
-          alignment: Alignment.center,
-          child: Text(
-            'Y',
-            style: TextStyle(color: Theme.of(context).colorScheme.primary),
-          ),
-        ),
-        SizedBox(width: 6),
-        Text(
-          ':\\',
-          style: TextStyle(
-            letterSpacing: 4,
-            color: Theme.of(context).colorScheme.onSurface.withAlpha(140),
-          ),
-        ),
-        Text('TANGERINE', style: TextStyle(letterSpacing: 4)),
-      ],
+      body: FutureBuilder(
+        future: itemRepo.fetchTopIds(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final itemId = snapshot.data![index];
+                return FutureBuilder(
+                  future: itemRepo.fetchItem(itemId),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.data != null) {
+                        final item = snapshot.data as Item;
+                        return ListTile(title: Text(item.title));
+                      } else {
+                        return SizedBox.shrink();
+                      }
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  },
+                );
+              },
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
